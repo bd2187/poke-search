1640cf1f3277634cdc8a92b4ec89c0de5f5979e1
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-19 20:32:11 -0700

    Set up initial state

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 
 const App = function() {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(false);
+
     return <h1>App</h1>;
 };
 
