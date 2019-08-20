52f35638acd59c97f9f9f072dcc3f1d703ee7e5f
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-19 21:36:54 -0700

    Call useEffect to fetch pokemon data. Import and render Loading, PokemonList, and Search components

--- src/app.js
+++ src/app.js
@@ -1,12 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { render } from "react-dom";
+
+import Loading from "./components/Loading";
+import PokemonList from "./components/PokemonList";
+import Search from "./components/Search";
 
 const App = function() {
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
     const [error, setError] = useState(false);
 
-    return <h1>App</h1>;
+    useEffect(function() {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(data) {
+                setTimeout(function() {
+                    setData(data.results);
+                    setLoading(false);
+                }, 2000);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <Router>
+            <div className="">
+                {loading ? <Loading /> : null}
+                {data.length > 0 ? (
+                    <>
+                        <Search />
+                        <PokemonList pokemon_arr={data} />
+                    </>
+                ) : null}
+            </div>
+        </Router>
+    );
 };
 
 render(<App />, document.getElementById("root"));
