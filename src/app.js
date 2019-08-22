c300e4f5f1431d03162d508c5e70ef15c1b9c4ba
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-21 21:50:04 -0700

    Add querieddata and searchValue variables to state. Ceate updateSearchValue fn which will display the results based on user's query

--- src/app.js
+++ src/app.js
@@ -9,7 +9,29 @@ import Search from "./components/Search";
 const App = function() {
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [queriedData, setQueriedData] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
     const [error, setError] = useState(false);
+
+    const updateSearchValue = function updateSearchValue(value) {
+        setSearchValue(value);
+
+        const lowerCasedQuery = value.toLowerCase().trim();
+        if (lowerCasedQuery === "") {
+            // display all pokemon
+            setQueriedData(data);
+        } else {
+            // filter through data and display results based on searchValue
+            const results = data.filter(function(pokemon) {
+                if (!pokemon.name) return;
+                const pokemon_name = pokemon.name.toLowerCase();
+
+                if (pokemon_name.includes(lowerCasedQuery)) return pokemon;
+            });
+
+            setQueriedData(results);
+        }
+    };
 
     useEffect(function() {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
@@ -19,6 +41,7 @@ const App = function() {
             .then(function(data) {
                 setTimeout(function() {
                     setData(data.results);
+                    setQueriedData(data.results);
                     setLoading(false);
                 }, 2000);
             })
@@ -30,13 +53,17 @@ const App = function() {
     return (
         <Router>
             <div className="">
-                {loading ? <Loading /> : null}
-                {data.length > 0 ? (
+                {loading ? (
+                    <Loading />
+                ) : (
                     <>
-                        <Search />
-                        <PokemonList pokemon_arr={data} />
+                        <Search
+                            searchValue={searchValue}
+                            updateSearchValue={updateSearchValue}
+                        />
+                        <PokemonList pokemon_arr={queriedData} />
                     </>
-                ) : null}
+                )}
             </div>
         </Router>
     );
