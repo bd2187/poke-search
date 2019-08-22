ee76da49221b14f1d3b24e53aef152040f8b1c25
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-21 21:51:25 -0700

    Add a condition to render the pokemon item only if pokemon_arr.length is greater than 0

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,26 +2,36 @@ import React from "react";
 const PokemonList = function({ pokemon_arr }) {
     return (
         <ul>
-            {pokemon_arr.map(function(pokemon_obj, index) {
-                var id = ++index;
-                var image_ext;
-                if (id <= 9) {
-                    image_ext = `00${id}`;
-                } else if (id >= 10 && id <= 99) {
-                    image_ext = `0${id}`;
-                } else {
-                    image_ext = id;
-                }
+            {pokemon_arr.length > 0
+                ? pokemon_arr.map(function(pokemon_obj) {
+                      var indexOfSubStr = pokemon_obj.url.indexOf("/pokemon/");
+                      var str = pokemon_obj.url.slice(
+                          indexOfSubStr,
+                          pokemon_obj.url.length
+                      );
+                      var id = parseInt(
+                          str.replace(/pokemon/g, "").replace(/\//g, "")
+                      );
 
-                var image_src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${image_ext}.png`;
+                      var image_ext;
+                      if (id <= 9) {
+                          image_ext = `00${id}`;
+                      } else if (id >= 10 && id <= 99) {
+                          image_ext = `0${id}`;
+                      } else {
+                          image_ext = id;
+                      }
 
-                return (
-                    <li key={`${pokemon_obj.name}__${id}`}>
-                        <p>{pokemon_obj.name}</p>
-                        <img src={image_src} />
-                    </li>
-                );
-            })}
+                      var image_src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${image_ext}.png`;
+
+                      return (
+                          <li key={`${pokemon_obj.name}__${id}`}>
+                              <p>{pokemon_obj.name}</p>
+                              <img src={image_src} />
+                          </li>
+                      );
+                  })
+                : null}
         </ul>
     );
 };
