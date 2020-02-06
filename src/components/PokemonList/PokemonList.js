0e7d6f536962e4447bab4beb01cec7873bf97fc7
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-05 21:44:35 -0800

    Initial commit. Create PokemonList component

---
+++ src/components/PokemonList/PokemonList.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+const PokemonList = ({ match }) => {
+    const [state, setState] = useState({
+        loading: true,
+        error: false,
+        pokemon: [],
+        offset: 0
+    });
+
+    const fetchPokemon = function() {
+        var endpoint = "";
+        {
+            let limit =
+                parseInt(match.params.offset) > 50
+                    ? parseInt(match.params.offset)
+                    : 50;
+
+            var endpoint =
+                parseInt(match.params.offset) > 0 && state.offset === 0
+                    ? `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
+                    : `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${state.offset}`;
+        }
+
+        fetch(endpoint)
+            .then(res => res.json)
+            .then(data =>
+                setState({
+                    loading: false,
+                    pokemon: [...state.pokemon, ...data.results],
+                    offset: state.offset + 50
+                })
+            )
+            .catch(() => setState({ loading: false, error: true }));
+    };
+
+    useEffect(function() {
+        fetchPokemon();
+
+        window.onscroll = function(ev) {
+            if (
+                window.innerHeight + window.scrollY >=
+                document.body.offsetHeight
+            ) {
+                console.warn("bottom");
+            }
+        };
+
+        return (window.onscroll = null);
+    }, []);
+
+    return null;
+};
+
+export default PokemonList;
