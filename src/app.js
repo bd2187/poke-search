67c1d3fabcc7e082def02de6e309ab2f6409bc31
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-09 17:11:25 -0800

    Add state to App component. Fetch pokemon when component mounts

--- src/app.js
+++ src/app.js
@@ -4,87 +4,68 @@ import { render } from "react-dom";
 
 import Loading from "./components/Loading";
 import PokemonList from "./components/PokemonList/PokemonList";
+import Navigation from "./components/Navigaton/Navigation";
 import Search from "./components/Search";
 
 const App = function() {
+    const [state, setState] = useState({
+        pokemon: [],
+        loading: true,
+        error: false
+    });
+
+    useEffect(function() {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+            .then(res => res.json())
+            .then(res => {
+                setState({
+                    pokemon: res.results,
+                    loading: false,
+                    error: false
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                setState({ error: true, loading: false });
+            });
+    }, []);
+
     return (
         <Router>
+            <Navigation />
             <Switch>
-                <Route exact path="/:offset?" component={PokemonList} />
+                <Route
+                    exact
+                    path="/"
+                    render={props => {
+                        return (
+                            <PokemonList
+                                {...props}
+                                pokemon={state.pokemon}
+                                loading={state.loading}
+                                error={state.error}
+                            />
+                        );
+                    }}
+                />
+
+                <Route
+                    exact
+                    path="/search/:query"
+                    render={props => {
+                        return (
+                            <PokemonList
+                                {...props}
+                                pokemon={state.pokemon}
+                                loading={state.loading}
+                                error={state.error}
+                            />
+                        );
+                    }}
+                />
             </Switch>
         </Router>
     );
 };
-
-// const App = function() {
-//     const [loading, setLoading] = useState(true);
-//     const [data, setData] = useState([]);
-//     const [queriedData, setQueriedData] = useState([]);
-//     const [searchValue, setSearchValue] = useState("");
-//     const [error, setError] = useState(false);
-
-//     const updateSearchValue = function updateSearchValue(value) {
-//         setSearchValue(value);
-
-//         const lowerCasedQuery = value.toLowerCase().trim();
-//         if (lowerCasedQuery === "") {
-//             // display all pokemon
-//             setQueriedData(data);
-//         } else {
-//             // filter through data and display results based on searchValue
-//             const results = data.filter(function(pokemon) {
-//                 if (!pokemon.name) return;
-//                 const pokemon_name = pokemon.name.toLowerCase();
-
-//                 if (pokemon_name.includes(lowerCasedQuery)) return pokemon;
-//             });
-
-//             setQueriedData(results);
-//         }
-//     };
-
-//     useEffect(function() {
-//         fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
-//             .then(function(res) {
-//                 return res.json();
-//             })
-//             .then(function(data) {
-//                 setData(data.results);
-//                 setQueriedData(data.results);
-//                 setLoading(false);
-//             })
-//             .catch(function(err) {
-//                 console.log(err);
-//             });
-//     }, []);
-
-//     // return (
-//     //     <Router>
-//     //         <div className="">
-//     //             {loading ? (
-//     //                 <Loading />
-//     //             ) : (
-//     //                 <>
-//     //                     <Search
-//     //                         searchValue={searchValue}
-//     //                         updateSearchValue={updateSearchValue}
-//     //                     />
-//     //                     <PokemonList pokemon_arr={queriedData} />
-//     //                 </>
-//     //             )}
-//     //         </div>
-//     //     </Router>
-//     // );
-
-//     return (
-//         <Router>
-//             <Switch>
-//                 <Route exact path="/:pageNumber?">
-//                     <Home />
-//                 </Route>
-//             </Switch>
-//         </Router>
-//     );
-// };
 
 render(<App />, document.getElementById("root"));
