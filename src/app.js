7f8ebcc6293bd3c8d7c339c217421d2b1b60b35d
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-17 19:14:03 -0800

    Import reset.css

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./styles/reset.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { render } from "react-dom";
 
