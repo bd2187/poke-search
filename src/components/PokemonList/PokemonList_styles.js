ade6bc7f4523ebf3ce9f56f81b7b73edffebe5db
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-17 19:34:14 -0800

    add grid styling

--- src/components/PokemonList/PokemonList_styles.js
+++ src/components/PokemonList/PokemonList_styles.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const List = styled.ul`
-    background-color: purple;
+    /* background-color: purple; */
+    display: grid;
+    grid-template-columns: 50% 50%;
+
+    li {
+        justify-self: center;
+        text-align: center;
+    }
 `;
