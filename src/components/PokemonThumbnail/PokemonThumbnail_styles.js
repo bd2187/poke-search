66d0a171366afcf83ae9a6af27897b62407ae392
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-20 21:58:44 -0800

    Initial commit. Declare styles for Thumbnail

---
+++ src/components/PokemonThumbnail/PokemonThumbnail_styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Thumbnail = styled.li`
+    justify-self: stretch;
+    text-align: center;
+    background-color: red;
+
+    img {
+        width: 100%;
+        height: auto;
+        display: block;
+        background: blue;
+    }
+`;
