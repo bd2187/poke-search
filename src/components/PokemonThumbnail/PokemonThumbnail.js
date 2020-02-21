fb1483363172caab60c084ac66786b2b45f14e41
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-20 21:58:21 -0800

    Set width and height attributes for img tag. Import Thumbnail styles

--- src/components/PokemonThumbnail/PokemonThumbnail.js
+++ src/components/PokemonThumbnail/PokemonThumbnail.js
@@ -2,17 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import prependZeroes from "../../utils/prependZeroes";
 import getPokemonID from "../../utils/getPokemonID";
+import { Thumbnail } from "./PokemonThumbnail_styles";
 
 const PokemonThumbnail = function({ pokemon }) {
     var id = prependZeroes(getPokemonID(pokemon.url));
     var imageURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`;
     return (
-        <li>
+        <Thumbnail>
             <Link to={`/pokemon/${pokemon.name}`}>
-                <img alt={pokemon.name} src={imageURL} />
+                <img
+                    alt={pokemon.name}
+                    src={imageURL}
+                    width="200"
+                    height="200"
+                />
                 <h1>{pokemon.name}</h1>
             </Link>
-        </li>
+        </Thumbnail>
     );
 };
 
