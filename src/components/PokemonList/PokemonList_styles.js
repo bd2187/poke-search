df77ca89deed9149c2045e345b924cc77d82ffa6
bd2187 <brandon.dionisio.26@gmail.com>
2020-02-20 21:59:25 -0800

    Update value of grid-template-columns. Remove styles for embedded li elements within ul

--- src/components/PokemonList/PokemonList_styles.js
+++ src/components/PokemonList/PokemonList_styles.js
@@ -3,10 +3,7 @@ import styled from "styled-components";
 export const List = styled.ul`
     /* background-color: purple; */
     display: grid;
-    grid-template-columns: 50% 50%;
-
-    li {
-        justify-self: center;
-        text-align: center;
-    }
+    grid-template-columns: 47.5% 47.5%;
+    grid-template-rows: auto;
+    grid-column-gap: 5%;
 `;
